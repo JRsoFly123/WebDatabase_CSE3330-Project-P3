782961b4fde60bc5f39a7f7beded5bd4bc246f44
JRsoFly123 <jeremiahrichard34@gmail.com>
2022-08-09 22:49:59 -0500

    Version 2.0 (DELETE function) - Added the delete students function for deleting self-support students with no milestone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import pymysql
@@ -17,7 +17,38 @@ def example():
     instructors = cursor.fetchall()
     return render_template("demo.html", instructors=instructors)
 
+# This function must:
+# - Add a student into the database
+# - Link the new student with an existing scholarship
+# - Add committee members for the student in the PhDCommittee table
+@app.route('/add_student', methods=['GET','POST'])
+def add_student():
+    if request.method == "POST":
+        # Fetching the data from the form
+        details = request.form
+        stuid = details['StudentId']
+        fName = details['FName']
+        lName = details['LName']
+        stSem = details['StSem']
+        stYear = details['StYear']
+        supervisor = details['Supervisor']
+        return 'success'
+    return render_template("add_student.html")
 
+@app.route('/del_students', methods=['GET','POST'])
+def del_ssstudent():
+    if request.method == "POST":
+        # Fetching the data from form
+        details = request.form
+        stuid = details['StudentId']
+        cursor = mysql.connection.cursor()
+        cursor.execute("DELETE st FROM phdstudent st WHERE EXISTS (SELECT * FROM selfsupport sst WHERE (sst.StudentId = "
+                       "st.StudentId) AND (sst.StudentId = '"+stuid+"') AND (st.StudentId NOT IN (SELECT StudentId "
+                                                                    "FROM milestonespassed)));")
+        mysql.connection.commit()
+        cursor.close()
+        return 'success'
+    return render_template("del_student.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
